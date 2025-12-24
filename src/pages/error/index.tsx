fcd5e09b9d5d05a7f37b4240caa1739cedb8ebd7
Wmbert <99267490+Wmbert@users.noreply.github.com>
2025-12-24 12:08:00 -0300

    end project

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router"
+
 export function ErrorPage(){
     return(
-        <main>
-            <h1>404</h1>
-            <h4>Página não existe</h4>
+        <main className="w-full h-screen flex flex-col items-center justify-center bg-violet-400">
+            <h1 className="text-9xl mt-10 font-bold italic text-white">
+                404!
+            </h1>
+
+            <h4 className="mt-4 text-2xl font-bold text-white italic">
+                Página não existe
+            </h4>
+
+            <Link 
+                className="mt-4 px-6 py-2 rounded font-bold bg-white"
+                to="/"
+            >
+                Voltar para home
+            </Link>
         </main>
     )
 }
